bfd1d7cf895077a01b2fe1d870b65bd3fa42817c
ncc-social <social@ncc.gov.gh>
2022-03-16 10:00:18 +0000

    Hi

--- erpnext_custom/server_scripts/leave_application.py
+++ erpnext_custom/server_scripts/leave_application.py
@@ -35,14 +35,14 @@ def after_approval(self):
     if self.status == "Open":
 			frappe.throw(_("Only Leave Applications with status 'Approved' and 'Rejected' can be submitted"))
 
-		self.validate_back_dated_application()
+	self.validate_back_dated_application()
 
-		# notify leave applier about approval
-		if frappe.db.get_single_value("HR Settings", "send_leave_notification"):
-			self.notify_employee()
+	# notify leave applier about approval
+	if frappe.db.get_single_value("HR Settings", "send_leave_notification"):
+		self.notify_employee()
 
-		self.create_leave_ledger_entry()
-		self.reload()
+	self.create_leave_ledger_entry()
+	self.reload()
 
 def validate_back_dated_application(self):
     future_allocation = frappe.db.sql("""select name, from_date from `tabLeave Allocation`
